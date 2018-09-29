11e5f80247efc95b329111df6ca51389887e8e89
Alena Kharytonchyk <alena.kharytonchyk@outlook.com>
2018-09-29 14:05:21 +0300

    The number of zeros is count

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+  let zerosCount = 0;
+  let nPow=1;
+  do {
+    countTemt = Math.floor(number / Math.pow(5, nPow));
+    zerosCount += countTemt;
+    nPow ++;
+  } while (countTemt >= 1);
+  return zerosCount;
 }
